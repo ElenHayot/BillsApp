dc03a8e167b7e0098989c6ac6843e7e5d8c4beb8
Elen Hayot <elen.hayot@gmail.com>
2025-12-29 10:48:58 +0100

    Initial Commit

---
+++ BillsApp/BillsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillsAppApp.swift
+//  BillsApp
+//
+//  Created by Elen Hayot on 29/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct BillsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BillsAppTests/BillsAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  BillsAppTests.swift
+//  BillsAppTests
+//
+//  Created by Elen Hayot on 29/12/2025.
+//
+
+import Testing
+
+struct BillsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
