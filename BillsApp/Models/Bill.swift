80d0996a24ba3ff105738a0c25eb2193b2d0a44a
Elen Hayot <elen.hayot@gmail.com>
2026-01-04 14:06:03 +0100

    Add bills model to read datas

--- BillsApp/Models/Bill.swift
+++ BillsApp/Models/Bill.swift
@@ -12,4 +12,38 @@ struct Bill: Identifiable, Decodable {
     let amount: Decimal
     let date: Date
     let categoryId: Int
+    let comment: String?
+    let createdAt: Date
+    let updatedAt: Date
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case title = "title"
+        case amount = "amount"
+        case date = "date"
+        case categoryId = "category_id"
+        case comment = "comment"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        title = try container.decode(String.self, forKey: .title)
+        date = try container.decode(Date.self, forKey: .date)
+        categoryId = try container.decode(Int.self, forKey: .categoryId)
+        comment = try container.decodeIfPresent(String.self, forKey: .comment)
+        createdAt = try container.decode(Date.self, forKey: .createdAt)
+        updatedAt = try container.decode(Date.self, forKey: .updatedAt)
+        
+        // Gère à la fois String et Number
+        if let stringValue = try? container.decode(String.self, forKey: .amount) {
+            amount = Decimal(string: stringValue) ?? 0
+        } else if let doubleValue = try? container.decode(Double.self, forKey: .amount) {
+            amount = Decimal(doubleValue)
+        } else {
+            amount = 0
+        }
+    }
 }
